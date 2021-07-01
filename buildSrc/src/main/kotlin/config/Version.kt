08b04c310efa1c30266efd9c4db9cbf0036ce24d
zhangcheng <906126890@qq.com>
2021-07-01 14:37:16 +0800

    通过buildSrc构建gradle

---
+++ buildSrc/src/main/kotlin/config/Version.kt
@@ -0,0 +1,18 @@
+package config
+
+/**
+ * @author ZC
+ * @date 2021/7/1
+ * desc:
+ */
+object Version {
+
+    const val kotlin_version = "1.5.0"
+    const val compileSdkVersion = 30
+    const val buildToolsVersion = "30.0.3"
+    const val minSdkVersion = 21
+    const val targetSdkVersion = 30
+    const val versionCode = 1
+    const val versionName = "1.0"
+
+}
